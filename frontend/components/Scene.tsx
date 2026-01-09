87eb27cdc45620c1d97e43eb66e24271b1f27f2f
saykathasan <139516340+saykathasan@users.noreply.github.com>
2026-01-09 16:04:30 +0530

    Refactor Scene component with box geometry
    Updated the Scene component to use a box geometry and adjusted lighting and camera position.

--- frontend/components/Scene.tsx
+++ frontend/components/Scene.tsx
@@ -1,17 +1,18 @@
-
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 
-export default function Scene(){
-  return(
-    <Canvas camera={{ position:[0,0,6] }}>
-      <ambientLight intensity={0.8}/>
-      <directionalLight position={[2,2,5]}/>
-      <mesh rotation={[0.5,0.5,0]}>
-        <icosahedronGeometry args={[1.5,1]}/>
-        <meshStandardMaterial color="#22c55e" wireframe/>
+export default function Scene() {
+  return (
+    <Canvas camera={{ position: [0, 0, 5] }}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[2, 2, 2]} />
+
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial color="#00ffcc" />
       </mesh>
-      <OrbitControls autoRotate enableZoom={false}/>
+
+      <OrbitControls />
     </Canvas>
-  )
+  );
 }
